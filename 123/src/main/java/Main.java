0dbc6497c995fa0018e3263eec02dffae85fc0d7
Alexey Ananichev <Ananichev@internet.ru>
2022-05-11 11:19:03 +0300

    1

--- 123/src/main/java/Main.java
+++ 123/src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("wtf");
+        System.out.println("wtf");
+        System.out.println("wtf");
     }
 }
